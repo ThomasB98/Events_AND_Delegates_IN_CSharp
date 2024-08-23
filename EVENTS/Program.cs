34bfc0968cc357259dae750e0567c0e4cc68779a
ThomasB98 <bansodthomas@gmail.com>
2024-08-23 13:03:58 +0530

    [Thomas]ADD: Basic Example of Event

---
+++ EVENTS/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EVENTS
+{
+    public delegate void Notify(string message);
+
+    public class EventPublisher
+    {
+        public event Notify OnNotify;
+
+        public void RaiseEvent(string message)
+        {
+            OnNotify?.Invoke(message);
+        }
+    }
+    public class EventSubscriber
+    {
+        public void OnEventRaised(string message)
+        {
+            Console.WriteLine("Event received: "+message);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            EventPublisher publisher = new EventPublisher();
+            EventSubscriber subscriber = new EventSubscriber();
+
+            publisher.OnNotify += subscriber.OnEventRaised;
+
+            publisher.RaiseEvent("test");
+        }
+    }
+}
---
+++ EVENTS/Temperature_Controller.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVENTS
+{
+    public delegate void TemperatureChangeHandler(string message);
+
+    public class TemperatureMonitor
+    {
+        public event TemperatureChangeHandler OnTemperatureChange;
+
+        private int _temperature;
+        
+
+        public int Temperature
+        {
+            get
+            {
+                return _temperature;
+            }
+
+            set
+            {
+                _temperature = value;
+
+                if(_temperature >35)
+                {
+                    //Raise EVENT!!
+                    RaiseTemperatureChangeEvent("Temperature is above threshold");
+                }
+            }
+        }
+
+        protected virtual void RaiseTemperatureChangeEvent(string message)
+        {
+            OnTemperatureChange?.Invoke(message);
+        }
+    }
+
+
+    public class TemperatueAlert 
+    {
+        public void OnTemperatureChange(string message)
+        {
+            Console.WriteLine("Alert: "+message);
+        }
+    }
+    internal class Temperature_Controller
+    {
+        static void Main(string[] args)
+        {
+            TemperatureMonitor monitor = new TemperatureMonitor();  
+            TemperatueAlert alert = new TemperatueAlert();
+
+            monitor.OnTemperatureChange += alert.OnTemperatureChange;
+
+            monitor.Temperature = 20;
+            Console.WriteLine("Please enter the temperature");
+            monitor.Temperature=int.Parse(Console.ReadLine());
+        }
+    }
+}
